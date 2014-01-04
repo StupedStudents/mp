9561d9ff86bdb2836965df82f14430140dd41f59
Logist- <pavelkrot@gmail.com>
2014-01-04 13:31:46 +0400

    Update

--- entry.c
+++
@@ -1,5 +0,0 @@
-void kernel_main(); // функция ядра
-
-void kernel_entry() {
-   kernel_main(); // передадим ей управление
-}
--- mne.bat
+++
@@ -1,6 +0,0 @@
-nasm -f win32 mne.asm -o mne1.o
-gcc -ffreestanding -masm=intel -c mne.c -o mne.o
-ld -Ttext=0x100 mne.o mne1.o -o mne.com
-objcopy mne.com -O binary
-
-
