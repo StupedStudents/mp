ecc6266e0972546e5f061a0b76c8998ed333986f
Randamm <hoos.spb@mail.ru>
2014-03-14 12:00:57 +0400

    test

--- main.bat
+++ main.bat
@@ -2,4 +2,4 @@ nasm -f win32 main.asm -o main1.o
 gcc -ffreestanding -masm=intel -c main.c -o main.o
 ld -Ttext=0x0700 main.o main1.o -o main.bin
 objcopy main.bin -O binary
-nasm -f bin boot.asm -o mbr.img 
+nasm -f bin boot.asm -o mbr.img
